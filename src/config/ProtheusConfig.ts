3662380ec6d20579495d68b524e8e03f72555f91
danieltorres31 <danieltorres31@outlook.com>
2021-01-10 18:25:29 -0300

    feat: Create ProtheusConfig

---
+++ src/config/ProtheusConfig.ts
@@ -0,0 +1,6 @@
+export default {
+    BASE_URL: process.env.PROTHEUS_BASE_URL || '',
+    USER: process.env.PROTHEUS_USER || '',
+    PASSWORD: process.env.PROTHEUS_PASSWORD || '',
+    PORTAL_TYPE: '2',
+}
